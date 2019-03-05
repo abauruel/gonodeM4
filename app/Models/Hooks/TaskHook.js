bc811a01dd9da2bab849527da44487fad9124d9d
Alex Claude <abauruel@gmail.com>
2019-03-05 11:12:08 -0300

    Adicionado gerenciamento de fila

--- app/Models/Hooks/TaskHook.js
+++ app/Models/Hooks/TaskHook.js
@@ -1,7 +1,6 @@
 'use strict'
-const Mail = use('Mail')
-const Helpers = use('Helpers')
-
+const Kue = use('Kue')
+const Job = use('App/Jobs/NewTaskMail')
 const TaskHook = (exports = module.exports = {})
 
 TaskHook.sendNewTaskMail = async taskInstance => {
@@ -10,20 +9,6 @@ TaskHook.sendNewTaskMail = async taskInstance => {
   const { username, email } = await taskInstance.user().fetch()
   const file = await taskInstance.file().fetch()
   const { title } = taskInstance
-  await Mail.send(
-    ['emails.new_task'],
-    { username, title, hasAttachment: !!file },
-    message => {
-      message
-        .to(email)
-        .from('abauruel@gmail.com', 'Alex Claude | Dev')
-        .subject('Nova tarefa para você')
 
-      if (file) {
-        message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
-          filename: file.name
-        })
-      }
-    }
-  )
+  Kue.dispatch(Job.key, { email, username, title, file }, { attemps: 3 })
 }
