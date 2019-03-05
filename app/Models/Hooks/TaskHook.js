abda5335e6c811e7bcd5206eb9d5a9dacd6a9793
Alex Claude <abauruel@gmail.com>
2019-03-05 10:33:48 -0300

    Adicionado envio de e-mail em nova tarefa

---
+++ app/Models/Hooks/TaskHook.js
@@ -0,0 +1,29 @@
+'use strict'
+const Mail = use('Mail')
+const Helpers = use('Helpers')
+
+const TaskHook = (exports = module.exports = {})
+
+TaskHook.sendNewTaskMail = async taskInstance => {
+  if (!taskInstance.user_id && !taskInstance.dirty.user_id) return
+
+  const { username, email } = await taskInstance.user().fetch()
+  const file = await taskInstance.file().fetch()
+  const { title } = taskInstance
+  await Mail.send(
+    ['emails.new_task'],
+    { username, title, hasAttachment: !!file },
+    message => {
+      message
+        .to(email)
+        .from('abauruel@gmail.com', 'Alex Claude | Dev')
+        .subject('Nova tarefa para você')
+
+      if (file) {
+        message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
+          filename: file.name
+        })
+      }
+    }
+  )
+}
